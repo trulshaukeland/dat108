4961a2545c5184d3850019272572ee9e6c05d628
Truls Agnar Haukeland <trulshaukeland@gmail.com>
2025-03-09 17:05:48 +0100

    Ekstra lambda opg, Eks1reduce.java

--- f05-streams2-fyllinn/src/no/hvl/dat108/f05/Eks1reduce.java
+++ f05-streams2-fyllinn/src/no/hvl/dat108/f05/Eks1reduce.java
@@ -2,13 +2,24 @@ package no.hvl.dat108.f05;
 
 import static no.hvl.dat108.f05.People.people;
 
+import java.util.stream.Collectors;
+
 public class Eks1reduce {
 	
 	public static void main(String[] args) {
 		
 		/* Summen av aldrene til personene i people-listen */
-		
-		/* En streng med alle initialene, "CD LC TC CB MA" */
+	        int totalAge = people.stream()
+            .mapToInt(p -> p.age())
+            .sum();
 
+        System.out.println("Total alder: " + totalAge);
+
+		/* En streng med alle initialene, "CD LC TC CB MA" */
+        String initials = people.stream()
+            .map(p -> p.firstName().charAt(0) + "" + p.lastName().charAt(0))
+            .collect(Collectors.joining(" "));
+
+        System.out.println("Initialer: " + initials);
 	}	
 }
