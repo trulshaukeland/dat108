d84a9d7de211a0378667e14a82d4dd57fb412841
Truls Agnar Haukeland <trulshaukeland@gmail.com>
2025-03-10 13:13:13 +0100

    Eks2countmatch

--- f05-streams2-fyllinn/src/no/hvl/dat108/f05/Eks2countmatch.java
+++ f05-streams2-fyllinn/src/no/hvl/dat108/f05/Eks2countmatch.java
@@ -7,13 +7,31 @@ public class Eks2countmatch {
 	public static void main(String[] args) {
 
 		//Alle forbokstavene i fornavnene i en streng "CLTCM" - reduce() 
-		
-		//Antall personer over 50 år - count()
-		
-		//Om vi har data som matcher - anyMatch(), allMatch(), noneMatch()
-		//Er alle over 30 år?
-		//Er noen over 60 år?
 
+		String alleForbokstaver = people.stream()
+		.map(p -> String.valueOf(p.firstName().charAt(0)))
+		.reduce("", (acc, letter) -> acc + letter);
+
+		System.out.println("Alle forbokstaver i alle navn: " + alleForbokstaver);
+		//Antall personer over 50 år - count()
+
+		double antallPersonerOver50 = people.stream()
+		.filter(p -> p.age() > 50)
+		.count();
+
+		System.out.println("Antall personer over 50: " + antallPersonerOver50);
+		
+		//Er alle over 30 år?
+
+		boolean alleOver30 = people.stream()
+		.allMatch(p -> p.age() > 30);
+
+		System.out.println("Alle over 30: " + alleOver30);
+		//Er noen over 60 år?
+		boolean noenOver60 = people.stream()
+		.anyMatch(p -> p.age() > 60);
+
+		System.out.println("Noen over 60: " + noenOver60);
 	}
 
 }
